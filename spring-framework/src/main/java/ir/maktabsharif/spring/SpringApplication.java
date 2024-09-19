7a8b89ba584db13f589efd73f568f78ac0e2a5fe
maxixking66 <maxixking66@gamil.com>
2024-09-19 09:49:14 +0330

    add AnnotationConfig example 1

--- spring-framework/src/main/java/ir/maktabsharif/spring/SpringApplication.java
+++ spring-framework/src/main/java/ir/maktabsharif/spring/SpringApplication.java
@@ -1,19 +1,23 @@
 package ir.maktabsharif.spring;
 
-import ir.maktabsharif.spring.service.FirstService;
+import ir.maktabsharif.spring.config.BeanConfig;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SpringApplication {
 
     public static void main(String[] args) {
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext(
-                "beans.xml"
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(
+                "ir.maktabsharif.spring"
         );
-        FirstService firstService = (FirstService) applicationContext.getBean("firstService");
-        firstService.afterInit();
-        System.out.println(firstService.getName());
-        firstService.getSecondService().print();
+
+        BeanConfig beanConfig = applicationContext.getBean(BeanConfig.class);
+        beanConfig.print();
+
+//        FirstService firstService = (FirstService) applicationContext.getBean("firstService");
+//        firstService.afterInit();
+//        System.out.println(firstService.getName());
+//        firstService.getSecondService().print();
     }
 
 }
---
+++ spring-framework/src/main/java/ir/maktabsharif/spring/config/BeanConfig.java
@@ -0,0 +1,11 @@
+package ir.maktabsharif.spring.config;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BeanConfig {
+
+    public void print() {
+        System.out.println("in BeanConfig print");
+    }
+}
