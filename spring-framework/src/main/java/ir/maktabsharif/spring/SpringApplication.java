ba8176b595f7274427abaed73806c6c5f00e3a29
maxixking66 <maxixking66@gamil.com>
2024-09-19 10:43:19 +0330

    add ComponentScan example 2

--- spring-framework/src/main/java/ir/maktabsharif/spring/SpringApplication.java
+++ spring-framework/src/main/java/ir/maktabsharif/spring/SpringApplication.java
@@ -1,15 +1,18 @@
 package ir.maktabsharif.spring;
 
-import ir.maktabsharif.spring.config.BeanConfig;
 import ir.maktabsharif.spring.service.ProductService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@ComponentScan
 public class SpringApplication {
 
     public static void main(String[] args) {
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(
-                BeanConfig.class
+                SpringApplication.class
         );
 
         ProductService firstService = applicationContext.getBean(ProductService.class);
--- spring-framework/src/main/java/ir/maktabsharif/spring/config/BeanConfig.java
+++
@@ -1,9 +0,0 @@
-package ir.maktabsharif.spring.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = "ir.maktabsharif.spring")
-public class BeanConfig {
-}
